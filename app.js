6e84acdc8dd1f31e4e804817038c385b7d9dbaa3
Emirhakan Tanhan <emirhakan.tanhan@gmail.com>
2020-11-07 13:38:52 +0300

    exp

--- app.js
+++ app.js
@@ -1,32 +1,9 @@
 new Vue({
     el:'#vue-app',
     data:{
-        name: 'emir',
-        job: 'DevOps',
-        website: 'http://www.emirhakan.me',
-        age: 19,
-        x: 0,
-        y: 0,
+        
      },
      methods: {
-         greet: function(time) {
-             return 'good ' + time + ' ' +this.name
-         },
-
-         add: function(year) {
-            this.age += year;
-            console.log("added " + year + " year.")
-         },
-         subtract: function(year) {
-             this.age -= year;
-             console.log("subtracted " + year + " year.")
-         },
-
-         updateXY: function(event) {
-             this.x = event.offsetX
-             this.y = event.offsetX
-
-         }
 
      },
 });
