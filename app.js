2b9fb1f2bb66c6deb31b348773a33915a589d4a3
Emirhakan Tanhan <emirhakan.tanhan@gmail.com>
2020-11-07 17:05:09 +0300

    Experimenting

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 new Vue({
     el:'#vue-app',
     data:{
-        
+        name: '',
+        age: '',
      },
      methods: {
-
+        logName: function(name) {
+            this.name = name
+        },
+        logAge: function(age) {
+            this.age = age
+        },
      },
 });
