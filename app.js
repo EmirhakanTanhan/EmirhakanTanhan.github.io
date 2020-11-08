ef3fbe3cc2fe197a0ef3c9e808276275d2d2f0bf
Emirhakan Tanhan <emirhakan.tanhan@gmail.com>
2020-11-08 03:00:06 +0300

    All Improved

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
 new Vue({
     el:'#vue-app',
-    data:{
-        name: '',
-        age: '',
-     },
-     methods: {
-        logName: function(name) {
-            this.name = name
+
+    data: {
+        health: 100,
+        ended: false,
+    },
+    methods: {
+        punch: function() {
+            this.health -= 10;
+            if(this.health <= 0) this.ended = true;
+            console.log(this.health)
         },
-        logAge: function(age) {
-            this.age = age
+
+        restart: function() {
+            this.health = 100;
+            this.ended = false;
         },
-     },
+    },
+    computed: {
+        
+    },
 });
