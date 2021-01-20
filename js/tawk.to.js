e90254b9266affc330af7af670e13440f05899b1
Emirhakan Tanhan <emirhakan.tanhan@gmail.com>
2021-01-20 15:58:13 +0300

    Added Tawk.to

---
+++ js/tawk.to.js
@@ -0,0 +1,12 @@
+<!--Start of Tawk.to Script-->
+var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
+(function () {
+    var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0];
+    s1.async = true;
+    s1.src = 'https://embed.tawk.to/60081ea4c31c9117cb7093f4/1esfrfdii';
+    s1.charset = 'UTF-8';
+    s1.setAttribute('crossorigin', '*');
+    s0.parentNode.insertBefore(s1, s0);
+})
+();
+<!--End of Tawk.to Script-->
